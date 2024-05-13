94e77e9bc7d894986a07e1fe98a2b315201ed100
aynazmeymanat <aynazmeymanat@gmail.com>
2024-05-13 14:03:43 +0330

    2st commit by A.Meymanat

--- src/FileAnalizer.java
+++ src/FileAnalizer.java
@@ -15,5 +15,27 @@ public class FileAnalizer {
         letterCounter = 0 ;
     }
 
+    public String getMaxWord() {
+        return maxWord;
+    }
 
+    public void setMaxWord(String maxWord) {
+        this.maxWord = maxWord;
+    }
+
+    public String getMinWord() {
+        return minWord;
+    }
+
+    public void setMinWord(String minWord) {
+        this.minWord = minWord;
+    }
+
+    public int getLetterCounter() {
+        return letterCounter;
+    }
+
+    public void setLetterCounter(int letterCounter) {
+        this.letterCounter = letterCounter;
+    }
 }
