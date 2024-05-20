7c8eade12386a834a372e4994cea6f7020aa802d
yarahmadi0077 <yarahmadi0077@gmail.com>
2024-05-20 13:28:00 +0330

    4st commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-//
+/\/
 public class Main {
     public static void main(String[] args) {
         FileAnalyzer fileAnalyzer1 = new FileAnalyzer("assets/file_1.txt");
